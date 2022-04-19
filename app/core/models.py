3be407e0d6d705c8b8f17456c06bc63f07be7f6a
Swapnil Sankla <swapnil.sankla@gmail.com>
2022-04-19 22:26:38 +0530

    Add customer user

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_field):
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_field):
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
