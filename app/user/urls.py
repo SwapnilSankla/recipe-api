c55cab40c024afb8916185e43acbbef8bb74dbc1
Swapnil Sankla <swapnil.sankla@gmail.com>
2022-04-20 16:23:09 +0530

    Add ability to retrieve user profile of logged-in user

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from user.views import CreateUserView, CreateTokenView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
+    path('me/', ManageUserView.as_view(), name='me'),
     path('token/', CreateTokenView.as_view(), name='token'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,11 +1,22 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+
+    def get_object(self):
+        return self.request.user
 
 
 class CreateTokenView(ObtainAuthToken):
